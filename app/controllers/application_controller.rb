02ebf6eb2f0a6ecaea56519bc7e71a166efaa347
Bruno Brito <bruno@git-tower.com>
2023-11-12 23:23:39 +0000

    Look for any HTML in tracklist folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   def index
     @covers = Dir.glob("app/assets/images/covers/*").shuffle.map do |f| File.basename f end
-    @tracklist = File.read("app/assets/tracklist/tracklist.html")
+
+    @tracklist = Dir.glob("app/assets/tracklist/*.html").first
+    tracklist_directory = "app/assets/tracklist/"
+    html_files = Dir.glob(File.join(tracklist_directory, "*.html"))
+
+    if html_files.any?
+      @tracklist = File.read(html_files.first)
+    else
+      # Handle the case where no HTML files are found
+      @tracklist = "No HTML files found in the directory."
+    end
+    
     gon.tracklist = @tracklist
   end
 end
